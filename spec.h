836a344d9a9e1b7ad321f6a48e85ebc761a711b5
Jian Weng <jian.weng@ucla.edu>
2021-07-12 16:31:43 -0700

    Make specification parameters loadable. (#9)

--- spec.h
+++ spec.h
@@ -10,6 +10,7 @@
 #pragma once
 
 #include <cstdint>
+#include <string>
 
 /*!
  * \brief The max number of DSA lanes supported
@@ -166,43 +167,9 @@ typedef uint64_t addr_t;
 namespace dsa {
 
 struct Specification {
-  /*!
-   * \brief The number of spatial lanes.
-   */
-  int num_of_lanes{8};
-  /*!
-   * \brief Cacheline width in bytes.
-   */
-  int cache_line{64};
-  /*!
-   * \brief The granularity of the decomposable spatial data path. By default it is
-   *        byte decomposable.
-   */
-  int dsa_granularity{1};
-  /*!
-   * \brief The number of multipliers that can be composed.
-   *        By default, 4 means, 0, 1, 2, 3, is 2^0, 2^1, 2^3, 2^3.
-   *        8-byte can be composed to byte, dual-byte, word, dual-word.
-   */
-  int dsa_composability{4};
-  /*!
-   * \brief The number of concurrent buffet streams.
-   */
-  int dsa_buffet_slots{4};
-
-  // TODO(@were): If it is a good idea that all the accelerators have the same number for this?
-  /*!
-   * \brief The size of the dsa commands that can be buffered.
-   */
-  int cmd_queue_size{16};
-  /*!
-   * \brief The width of issue window.
-   */
-  int cmd_issue_width{4};
-  /*!
-   * \brief If the issue is out of order.
-   */
-  bool cmd_issue_ooo{true};
+#define SPEC_ATTR(TY, ID, VAL) TY ID{VAL};
+#include "./spec.attr"
+#undef SPEC_ATTR
 };
 
 }
